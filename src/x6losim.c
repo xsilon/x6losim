6df3cc7bc0b283f7940cf8a97881df2ff4a43ff0
Martin Townsend <martin.townsend@xsilon.com>
2014-10-17 16:15:22 +0100

    Added autotools files and example hello world program

---
+++ src/x6losim.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : x6losim.c
+ Author      : Martin Townsend
+ Version     :
+ Copyright   : All Rights Reserved Xsilon Ltd 2014.
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
